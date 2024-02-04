4170c8a140758f9231038dbffad7ce007321c43a
Heizahn <humbertorbw@gmail.com>
2024-02-04 19:34:15 -0400

    addition, subtraction, multiplication and division functions completed to be operated.

---
+++ main.js
@@ -0,0 +1,18 @@
+function suma(num1, num2) {
+	return num1 + num2
+}
+
+function resta(num1, num2) {
+	return num1 - num2
+}
+
+function multi(num1, num2) {
+	return num1 * num2
+}
+
+function divi(num1, num2) {
+	if (num2 === 0) return "Infinity"
+
+	return num1 / num2
+}
+
